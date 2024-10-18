834612dbea4e17510a5feb72d162ed4ae9a6eaae
Your Name <you@example.com>
2024-10-18 17:30:18 +0700

    update

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import numpy as np
-import copy
-import pickle
-import matplotlib.pyplot as plt
-import os
+import yaml
 
-from TicTacToeBoard import TicTacToeBoard
 from TrainStrategy.BasePlayer import BasePlayer
 from TrainStrategy.QLearning import RLAgentQLearning
 from Game import *
+from utils import *
 
-board_size = 4
-number_of_games = 5000
+with open("config.yml", 'r') as f:
+    dict_config = yaml.load(f, Loader=yaml.FullLoader)
 
-training_step_test = [100, 1000, 5000, 10000, 20000, 100000]
+args = dict2namespace(dict_config)
+args = args.main_game
 
-episodes = np.max(np.array(training_step_test))
+print(dict_config)
+print(args)
 
 
-rl_agent = RLAgentQLearning("QLearning".format(training_step_test[-1]), "blue", board_size)
+episodes = np.max(np.array(args.training_step_test))
 
 
-random_opp = BasePlayer("Random", "red", board_size)
+rl_agent = RLAgentQLearning("QLearning".format(args.training_step_test[-1]), "blue", args.board_size)
 
-game(rl_agent, random_opp, board_size, print_board=True, debug=True)
 
-multiple_game_multiple_step_train(rl_agent, training_step_test, board_size, number_of_games)
+random_opp = BasePlayer("Random", "red", args.board_size)
 
-qlearning_agent = RLAgentQLearning("QLearning".format(training_step_test[-1]), "blue", board_size)
-qlearning_agent.load_policy("./Q-Learning/size={}/QLearning_epoch={}_policy.pkl".format(board_size, 20000))
+game(rl_agent, random_opp, args.board_size, print_board=True, debug=True)
 
-sarsa_agent = RLAgentQLearning("SARSA".format(training_step_test[-1]), "green", board_size)
-sarsa_agent.load_policy("./SARSA/size={}/SARSA_epoch={}_policy.pkl".format(board_size, 20000))
+multiple_game_multiple_step_train(
+    rl_agent,
+    args.training_step_test,
+    args.board_size,
+    args.number_of_games)
+
+qlearning_agent = RLAgentQLearning(
+    "QLearning".format(args.training_step_test[-1]),
+    "blue", args.board_size)
+qlearning_agent.load_policy("./Q-Learning/size={}/QLearning_epoch={}_policy.pkl".format(args.board_size, 20000))
+
+sarsa_agent = RLAgentQLearning("SARSA".format(args.training_step_test[-1]), "green", args.board_size)
+sarsa_agent.load_policy("./SARSA/size={}/SARSA_epoch={}_policy.pkl".format(args.board_size, 20000))
 
 qlearning_wins = 0
 
@@ -39,8 +47,8 @@ sarsa_wins = 0
 draw = 0
 
 for i in range(5000):
-    _, res1 = game(qlearning_agent, sarsa_agent, board_size, print_board=True, debug=True)
-    _, res2 = game(sarsa_agent, qlearning_agent, board_size, print_board=True, debug=True)
+    _, res1 = game(qlearning_agent, sarsa_agent, args.board_size, print_board=True, debug=True)
+    _, res2 = game(sarsa_agent, qlearning_agent, args.board_size, print_board=True, debug=True)
 
     qlearning_wins += 1 if res1 == X_WIN else 0
     qlearning_wins += 1 if res2 == O_WIN else 0
